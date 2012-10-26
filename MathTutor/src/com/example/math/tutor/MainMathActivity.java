7262b5835504fd71d5cedad3cbecb09f203e86a6
Matthew McNew <matthew@mcnew.(none)>
2012-10-26 12:34:42 -0600

    Added gesture listening

--- MathTutor/src/com/example/math/tutor/MainMathActivity.java
+++ MathTutor/src/com/example/math/tutor/MainMathActivity.java
@@ -1,15 +1,34 @@
 package com.example.math.tutor;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.gesture.Gesture;
+import android.gesture.GestureLibraries;
+import android.gesture.GestureLibrary;
+import android.gesture.GestureOverlayView;
+import android.gesture.GestureOverlayView.OnGesturePerformedListener;
+import android.gesture.Prediction;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Toast;
 
-public class MainMathActivity extends Activity {
+public class MainMathActivity extends Activity implements OnGesturePerformedListener {
+	
+    private GestureLibrary mLibrary;
 
-    @Override
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_math);
+        
+        mLibrary = GestureLibraries.fromRawResource(this, R.raw.gestures);
+        if (!mLibrary.load()) {
+          finish();
+        }
+        
+        GestureOverlayView gestures = (GestureOverlayView) findViewById(R.id.gestures);
+        gestures.addOnGesturePerformedListener(this);
     }
 
     @Override
@@ -17,4 +36,22 @@ public class MainMathActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main_math, menu);
         return true;
     }
+
+	@Override
+	public void onGesturePerformed(GestureOverlayView overlay, Gesture gesture) {
+		   ArrayList<Prediction> predictions = mLibrary.recognize(gesture);
+
+		   if (predictions.size() > 0 && predictions.get(0).score > 1.0) {
+		     int result = Integer.valueOf(predictions.get(0).name);
+
+		     
+		       Toast.makeText(this, "" + result, Toast.LENGTH_LONG).show();
+		
+		   } else {
+			   Toast.makeText(this, "Couldn't Recognize. Sorry Sport.", Toast.LENGTH_LONG).show();   
+		   }
+			   
+			   
+		
+	}
 }
